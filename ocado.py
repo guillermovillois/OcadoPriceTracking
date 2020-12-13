844ad186862dc48f135c1bad18b6861e033cb614
Guille <guillearg@gmail.com>
2020-12-13 10:03:23 -0300

    get products on page

--- ocado.py
+++ ocado.py
@@ -5,6 +5,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
 
 # base url
 url = 'https://www.ocado.com/webshop/startWebshop.do'
@@ -32,3 +45,17 @@ subcats = []
 for each in driver.find_element_by_class_name('grocery-section').find_elements_by_class_name('level-item-link'):
     subcats.append(each.get_attribute('href'))
     print(each.get_attribute('href'), each.get_attribute('text'))
+
+while check_exists_by_xpath("//button[@class='btn-primary show-more']"):
+    fBody.click()
+    print(len(driver.find_elements_by_class_name('fops-item')))
+
+
+products_on_page = BeautifulSoup(
+    driver.page_source, 'html.parser').findAll('div', class_='fop-item')
+for product in products_on_page[:20]:
+    print(product.find('h4', {'class': 'fop-title'}).attrs['title'])
+    print(product.attrs['data-sku'])
+    print(product.find('div', class_='price-group-wrapper').find('span',
+                                                                 {'class': 'fop-price'}).text)
+    print('https://www.ocado.com' + product.find('a')['href'])
